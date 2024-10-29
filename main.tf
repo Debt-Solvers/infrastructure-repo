682ba5eb56ebfe847da20aef5aa2fd51b53bbb5d
hzhao101 <zhaohongwei14@hotmail.com>
2024-10-29 18:04:05 -0400

    second commit, added code of Vnet and VM

--- main.tf
+++ main.tf
@@ -2,39 +2,43 @@
 provider "azurerm" {
   features {}
 
-  # Azure details, replace with sub IP
-  subscription_id = "replace"
+  # Azure details, replace with sub IP or use env
+#  subscription_id = "replace"
 }
 
 # Create a resource group
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "my_rg" {
   name     = "debtSolverRG"
   location = "East US"
 }
 
 # az login --use-device-code
+#$env:ARM_SUBSCRIPTION_ID = "<your_subscription_id>"
+#$env:ARM_CLIENT_ID = "<your_client_id>"
+#$env:ARM_CLIENT_SECRET = "<your_client_secret>"
+#$env:ARM_TENANT_ID = "<your_tenant_id>"
 
 resource "azurerm_virtual_network" "my_vnet" {
-  name                = "myVnet"
+  name                = "appVnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.my_rg.location
   resource_group_name = azurerm_resource_group.my_rg.name
 }
 
 resource "azurerm_subnet" "my_subnet" {
-  name                 = "mySubnet"
+  name                 = "vmSubnet"
   resource_group_name  = azurerm_resource_group.my_rg.name
   virtual_network_name = azurerm_virtual_network.my_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_interface" "my_nic" {
-  name                = "myNIC"
+  name                = "vmNIC"
   location            = azurerm_resource_group.my_rg.location
   resource_group_name = azurerm_resource_group.my_rg.name
 
   ip_configuration {
-    name                          = "myIPConfig"
+    name                          = "vmIPConfig"
     subnet_id                     = azurerm_subnet.my_subnet.id
     private_ip_address_allocation = "Dynamic"
   }
@@ -48,12 +52,13 @@ resource "azurerm_linux_virtual_machine" "my_vm" {
 
   admin_username = "azureuser"
   admin_password = "YourP@ssword1234"
+  disable_password_authentication = false
 
   network_interface_ids = [azurerm_network_interface.my_nic.id]
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    storage_account_type = "StandardSSD_LRS"
   }
 
   source_image_reference {

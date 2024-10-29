0a764eb645ace30fe4c1afb3a6d873a5325947bd
hzhao101 <zhaohongwei14@hotmail.com>
2024-10-29 17:38:27 -0400

    README added

---
+++ README.md
@@ -0,0 +1,5 @@
+# az login --use-device-code
+#$env:ARM_SUBSCRIPTION_ID = "<your_subscription_id>"
+#$env:ARM_CLIENT_ID = "<your_client_id>"
+#$env:ARM_CLIENT_SECRET = "<your_client_secret>"
+#$env:ARM_TENANT_ID = "<your_tenant_id>"
--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 provider "azurerm" {
   features {}
 
-  # Azure details
+  # Azure details, replace with sub IP
   subscription_id = "replace"
 }
 
@@ -13,3 +13,53 @@ resource "azurerm_resource_group" "example" {
 }
 
 # az login --use-device-code
+
+resource "azurerm_virtual_network" "my_vnet" {
+  name                = "myVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.my_rg.location
+  resource_group_name = azurerm_resource_group.my_rg.name
+}
+
+resource "azurerm_subnet" "my_subnet" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.my_rg.name
+  virtual_network_name = azurerm_virtual_network.my_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "my_nic" {
+  name                = "myNIC"
+  location            = azurerm_resource_group.my_rg.location
+  resource_group_name = azurerm_resource_group.my_rg.name
+
+  ip_configuration {
+    name                          = "myIPConfig"
+    subnet_id                     = azurerm_subnet.my_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "my_vm" {
+  name                = "myVM"
+  resource_group_name = azurerm_resource_group.my_rg.name
+  location            = azurerm_resource_group.my_rg.location
+  size                = "Standard_B1s"
+
+  admin_username = "azureuser"
+  admin_password = "YourP@ssword1234"
+
+  network_interface_ids = [azurerm_network_interface.my_nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}

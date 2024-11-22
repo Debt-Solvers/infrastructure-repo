faa12fd9e9fd2d4d99e229f2bd7b6043e85b77da
gatsbynguyen99 <gatsbynguyencanada@gmail.com>
2024-11-22 01:32:24 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -3,6 +3,7 @@ output "vm_public_ip" {
   value       = azurerm_public_ip.my_public_ip.ip_address
 }
 
-output "acr_login_server" {
-  value = azurerm_container_registry.my_acr.login_server
+output "kind_public_ip" {
+  description = "The public IP address of the kind VM"
+  value       = azurerm_public_ip.my_public_ip_kind.ip_address
 }

5c9b591d00abcc0a4c1f33682a7653de7e360339
gatsbynguyen99 <gatsbynguyencanada@gmail.com>
2024-11-22 09:00:46 -0500

    Update install_docker_and_kind.sh

--- install_docker_and_kind.sh
+++ install_docker_and_kind.sh
@@ -14,36 +14,32 @@ sudo apt-get install -y docker-ce
 sudo usermod -aG docker azureuser
 
 # Install Kind
-curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64"
-chmod +x ./kind
-sudo mv ./kind /usr/local/bin/kind
+curl -sLo kind https://kind.sigs.k8s.io/dl/v0.11.0/kind-linux-amd64
+sudo install -o root -g root -m 0755 kind /usr/local/bin/kind
+rm -f ./kind
 
-# Install kubectl
+# Install Kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm -f ./kubectl
 
-# Create Kind Cluster with Port Mappings
-cat <<EOF | kind create cluster --config=-
+# Create configuration file for Kind cluster
+cat <<EOF > /home/azureuser/kind.yaml
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
-  - role: control-plane
-    image: kindest/node:v1.21.1
-    extraPortMappings:
-      - containerPort: 30000
-        hostPort: 30000
-        protocol: TCP
-      - containerPort: 30001
-        hostPort: 30001
-        protocol: TCP
+- role: control-plane
+  image: kindest/node:v1.19.11@sha256:07db187ae84b4b7de440a73886f008cf903fcf5764ba8106a9fd5243d6f32729
+  extraPortMappings:
+  - containerPort: 30000
+    hostPort: 30000
+  - containerPort: 30001
+    hostPort: 30001
 EOF
 
-# Verify installations
-docker --version
-kind --version
-kubectl version --client --output=yaml
+sudo chown azureuser:azureuser /home/azureuser/kind.yaml
+sleep 5
 
-# Output node status to verify cluster setup
-kubectl get nodes
+# Create cluster with kind.yaml
+sudo -u azureuser /usr/local/bin/kind create cluster --config /home/azureuser/kind.yaml
+echo "Script completed."
